8d8c1800063c6afd0580912b2cddda963047c708
jihyyeshin <tlsjh082@gmail.com>
2019-03-28 21:37:11 +0900

    final

---
+++ 03주차/Project/src/ThreadExample2.java
@@ -0,0 +1,11 @@
+
+public class ThreadExample2 {
+	public static void main(String[] args) {
+		Thread thread =new MovieThread2();
+		thread.setDaemon(true);
+		thread.start();
+		
+		try {Thread.sleep(3000);} catch (InterruptedException e) {}
+		System.out.println("메인 종료");
+	}
+}
