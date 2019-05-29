272ba899302842f24b3140fdb43a88234ec3c1e3
jihyyeshin <tlsjh082@gmail.com>
2019-05-29 23:51:23 +0900

    190529 edit study file

---
+++ Mybatis/Project/src/main/java/kr/co/java/Project/App.java
@@ -0,0 +1,13 @@
+package kr.co.java.Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
