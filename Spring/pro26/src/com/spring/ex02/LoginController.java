133bc892cffb57d965c825d7a809f2d7a9632b02
jihyyeshin <tlsjh082@gmail.com>
2019-07-18 17:47:18 +0900

    190718 annotation

--- Spring/pro26/src/com/spring/ex02/LoginController.java
+++ Spring/pro26/src/com/spring/ex02/LoginController.java
@@ -34,7 +34,7 @@ public class LoginController {
 
 		return mav;
 	}
-
+    /*
 	@RequestMapping(value = "/test/login2.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public ModelAndView login2(@RequestParam("userID") String userID, 
 			                  @RequestParam("userName") String userName,
@@ -54,8 +54,10 @@ public class LoginController {
 
 		return mav;
 	}
-	
-/*	
+	*/
+	//required를 true로 하면 반드시 매개변수를 전달해야함. 디폴트가 true
+	//required를 false로 하면 매개변수를 전달하지 않는 경우 null을 할당
+
 	@RequestMapping(value = "/test/login2.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public ModelAndView login2(@RequestParam("userID") String userID, 
                                @RequestParam(value="userName", required=true) String userName,
@@ -76,9 +78,10 @@ public class LoginController {
 		return mav;
 	}
 	
-*/	
-	/*@RequestMapping(value = "/test/login3.do", method = { RequestMethod.GET, RequestMethod.POST })
-	public ModelAndView login3(@RequestParam Map<String, String> info,
+
+	//일일이 보내지 않고 map에 저장
+	@RequestMapping(value = "/test/login3.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView login3(@RequestParam Map<String, String> info,//Map을 생성
 			                   HttpServletRequest request, HttpServletResponse response) throws Exception {
 		request.setCharacterEncoding("utf-8");
 		ModelAndView mav = new ModelAndView();
@@ -88,12 +91,13 @@ public class LoginController {
 		System.out.println("userID: "+userID);
 		System.out.println("userName: "+userName);
 		
-		mav.addObject("info", info);
+		mav.addObject("info", info);//info object생성
 		mav.setViewName("result");
 		return mav;
-	}*/
+	}
 	
-	/*@RequestMapping(value = "/test/login4.do", method = { RequestMethod.GET, RequestMethod.POST })
+	//VO 클래스의 속성과 동일한 속성, jsp파일에서 info를 이용해 바로 loginVO속성에 접근할 수 있다.
+	@RequestMapping(value = "/test/login4.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public ModelAndView login4(@ModelAttribute("info") LoginVO loginVO,
 			                   HttpServletRequest request, HttpServletResponse response) throws Exception {
 		request.setCharacterEncoding("utf-8");
@@ -102,12 +106,13 @@ public class LoginController {
 		System.out.println("userName: "+loginVO.getUserName());
 		mav.setViewName("result");
 		return mav;
-	}*/
+	}
 	   
 	@RequestMapping(value = "/test/login5.do", method = { RequestMethod.GET, RequestMethod.POST })
 	public String login5(Model model,
 			                   HttpServletRequest request, HttpServletResponse response) throws Exception {
 		request.setCharacterEncoding("utf-8");
+		//jsp에 전달할 데이터를 model에 addAttribute를 이용하여 바인딩
 		model.addAttribute("userID", "hong");
 		model.addAttribute("userName", "홍길동");
 		return "result";
